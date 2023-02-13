639163f751626392ac96efd5fda11fe80ffc65a7
David Claphan <97066227+davidclaphan@users.noreply.github.com>
2023-02-12 16:49:47 -0800

    reorganized files and added microservice

---
+++ micro.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const phrases = [
+    "Drink more water!",
+    "Get a full 8 hours!",
+    "Make sure you eat your vegetables!",
+    "30 minutes of exercise a day!",
+    "Go for a walk",
+    "Put down your phone to limit your screen time!",
+    "Make time to relax"
+];
+
+module.exports.phrases = phrases;
---
+++ server.js
@@ -0,0 +1,33 @@
+// HEADER
+'use strict';
+
+const PORT = 3000;
+
+const phrases = require('./micro.js').phrases;
+
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+// functions
+
+function motivationalPhrases(){
+    let num = Math.floor(Math.random() * 7);
+    return phrases[num];
+}
+
+app.post("/motivation", (req, res) => {
+    let suggestion = motivationalPhrases();
+    res.send(suggestion);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
